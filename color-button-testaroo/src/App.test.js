fc8e7c7ace45e9ffe7e18dca09860c449c9fa3d0
Jack Perry <jack.e.perry@outlook.com>
2021-12-18 09:04:53 -0700

    👌 IMPROVE:

--- color-button-testaroo/src/App.test.js
+++ color-button-testaroo/src/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('button has correct initial color and changes when clicked', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const colorBtn = screen.getByRole('button', { name: /Change to blue/ });
+
+  // Check Style
+  expect(colorBtn).toHaveStyle({ backgroundColor: 'red' });
+
+  // Click Button
+  fireEvent.click(colorBtn);
+
+  // Check Style
+  expect(colorBtn).toHaveStyle({ backgroundColor: 'blue' });
+
+  // Check Button Text
+  expect(colorBtn.textContent).toBe('Change to red');
+});
+
+test('Initial conditions', () => {
+  render(<App />);
+
+  // check that btn starts enabled
+  const colorBtn = screen.getByRole('button', { name: /Change to blue/ });
+  expect(colorBtn).toBeEnabled();
+
+  // checkbox is unchecked
+  const checkbox = screen.getByRole('checkbox');
+  expect(checkbox).not.toBeChecked();
 });
